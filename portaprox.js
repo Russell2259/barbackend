38f46a3272a4024feda632eeaf252617359f9f27
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-05-22 18:44:22 -0600

    Update portaprox.js

--- portaprox.js
+++ portaprox.js
@@ -1,2 +1,41 @@
-/* UBer v4.11 by luphoria */
-!function(){var t=prompt("Welcome to the BAR Porta-Proxy tool v4.11\nenter url, the one already there is Google. make sure to start with http://, https://, or //.\nOR enter 'p' to go to a proxy site.\n\n\n-BAR Developer Team",placeholder="//www.google.com/?igu=1");if("p"==t){var e=prompt("Welcome to BAR Porta-Proxy tool v4.11\nenter url.-BAR Developer Team",placeholder="https://google.com/");""!=e&&null!=e&&(e.includes("//",0)||e.includes("http://",0)||e.includes("https://",0)?t="https://ctaetcsh.lgbt/fetch/"+encodeURIComponent(e.toString().split("").map((t,e)=>e%2?String.fromCharCode(2^t.charCodeAt()):t).join("")):(alert("invalid url, quitting.\nmake sure to start with 'http://', 'https://', or '//'."),t=null,e=null))}""!=t&&null!=t&&(t.includes("//",0)||t.includes("http://",0)||t.includes("https://",0)?(document.body.innerHTML='<body style="margin:0px;padding:0px;overflow:hidden"><iframe src="'+t+'" id="uberFrame" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe></body>',window.onbeforeunload=function(){return console.log("Lyft implementation courtesy of the BAR Porta-Proxy tool"),"the funny"}):alert("invalid url, quitting.\nmake sure to start with 'http://', 'https://', or '//'."))}();
+/* 
+  UBer v4.11 by luphoria.
+*/
+/* javascript: */(function() {
+  var url = prompt("\
+Welcome to UBer v4.11\n\
+enter url, the one already there is Google. make sure to start with http://, https://, or //.\n\
+OR enter 'p' to go to a proxy site.\n\n\n\
+-luphoria",
+    placeholder = "//www.google.com/?igu=1") /* google with flag used by agoogleaday -- allows google.com for cross-origin iframe */
+  if (url == "p") {
+    /* if user inputs "p" instead of a url */
+    var url2 = prompt("\
+Welcome to UBer v4.11\n\
+enter url.\
+-luphoria",
+      placeholder = "https://google.com/") /* flag not required when using proxy */
+    if (url2 != "" && null != url2) /* cancel check */ {
+      if (url2.includes("//", 0) || url2.includes("http://", 0) || url2.includes("https://", 0)) {
+        url = "https://ht.luphoria.com:3301/service/" + encodeURIComponent(url2.toString().split('').map((char, ind) => ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char).join('')) // XOR encoding -- taken from codec.js in the Corrosion source, but most likely a standard(?)
+      } else {
+        alert("invalid url, quitting.\nmake sure to start with 'http://', 'https://', or '//'.")
+        url = null
+        url2 = null
+      }
+    }
+  }
+  
+  if ("" != url && null != url) {
+    if (url.includes("//", 0) || url.includes("http://", 0) || url.includes("https://", 0)) {
+      document.body.innerHTML = '<body style="margin:0px;padding:0px;overflow:hidden"><iframe src="' + url + '" id="uberFrame" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe></body>'
+      window.onbeforeunload = function() {
+        /* lyft implementation built-in Just in Case(tm) :) */
+        console.log("Lyft implementation courtesy of UBer")
+        return "the funny"
+      }
+    } else {
+      alert("invalid url, quitting.\nmake sure to start with 'http://', 'https://', or '//'.")
+    }
+  }
+})()
